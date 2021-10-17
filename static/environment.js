c24ed5f07573569bc11a93f3500951727a2d9d52
Nicolas Grobelny <miki4920@gmail.com>
2021-10-17 18:21:29 +0100

    Added a function for detecting mouse position

--- static/environment.js
+++ static/environment.js
@@ -1,9 +1,21 @@
 let parent;
 let canvas;
-
 window.onload = window.onresize = function() {
     canvas = document.getElementById('camera_canvas');
     parent = document.getElementById('camera_container')
     canvas.width = parent.innerWidth;
     canvas.height = parent.innerHeight;
 }
+
+function print_position(event) {
+    console.log(event["layerX"] + "," + event["layerY"])
+}
+
+function draw() {
+    canvas = document.getElementById('camera_canvas');
+    canvas.addEventListener("mousedown", function(e) {
+        print_position(e)
+    })
+}
+
+draw()
