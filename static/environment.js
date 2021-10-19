55d73776775f5c97df44216f28fc5ffae493a021
Nicolas Grobelny <miki4920@gmail.com>
2021-10-19 22:50:56 +0100

    Added an option for removing environment elements

--- static/environment.js
+++ static/environment.js
@@ -2,7 +2,7 @@ let canvas;
 let set = new Set();
 let pixel_resolution = 40;
 
-let drawing = false;
+let drawing_mode = 0;
 let x;
 let y;
 let context;
@@ -10,33 +10,47 @@ let context;
 
 window.onload = window.onresize = function() {
     canvas = document.getElementById('camera_canvas')
-
     canvas.width = canvas.clientWidth - (canvas.clientWidth % pixel_resolution);
     canvas.height = canvas.clientHeight - (canvas.clientHeight % pixel_resolution);
 }
 
-function draw_pixel(canvas, socket, event) {
+function draw_pixel(canvas, socket, colour, event) {
     x = event["layerX"] - (event["layerX"] % pixel_resolution)
     y = event["layerY"] - (event["layerY"] % pixel_resolution)
     context = canvas.getContext("2d")
-    context.fillStyle = "white";
+    context.fillStyle = colour;
     context.fillRect(x, y, pixel_resolution,pixel_resolution);
-    set.add(x/pixel_resolution + "," + y/pixel_resolution)
+    return x/pixel_resolution + "," + y/pixel_resolution
 }
 
 function draw(socket) {
     canvas = document.getElementById('camera_canvas');
     canvas.addEventListener('contextmenu', event => event.preventDefault())
     canvas.addEventListener("mousedown", function(e) {
-
-        drawing = true;
+        switch(e.button) {
+            case 0:
+                drawing_mode = 1
+                break
+            case 2:
+                drawing_mode = 2
+                break
+            default:
+                drawing_mode = 0
+        }
     })
     canvas.addEventListener("mouseup", function(e) {
-        drawing = false;
+        drawing_mode = 0;
     })
     canvas.addEventListener("mousemove", function(e) {
-        if(drawing) {
-            draw_pixel(canvas, socket, e)
+        if(drawing_mode === 1) {
+            let element = draw_pixel(canvas, socket, "white", e)
+            set.add(element)
+        }
+        else if(drawing_mode === 2) {
+            let element = draw_pixel(canvas, socket, "rgb(18, 18, 18)", e)
+            if (set.has(element)) {
+                set.delete(element)
+            }
         }
     })
 }
