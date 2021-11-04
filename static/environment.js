716235a4b5f2bede49f95a9864d1bc92013a8703
Nicolas Grobelny <miki4920@gmail.com>
2021-11-04 17:46:31 +0000

    Introduced a samplign mechanism

--- static/environment.js
+++ static/environment.js
@@ -3,16 +3,20 @@ let width;
 let height;
 
 let pixel_resolution = [1, 1];
-let default_size = 40;
+let default_size = 10;
 
 let dictionary = {};
-let drawing_colour = Colours["EMPTY"];
+let drawing_colour = "WALL";
 let drawing = false;
 
 let x;
 let y;
 let context;
 
+
+function change_colour(colour) {
+    drawing_colour = colour;
+}
 
 function update_resolution(change_dimensions) {
     canvas = document.getElementById('camera_canvas')
@@ -34,37 +38,30 @@ function resize_window(change_dimensions=false) {
 }
 
 
-function change_colour(colour) {
-    drawing_colour = Colours[colour];
-}
-
 
 function fill_canvas(canvas) {
-    let context = canvas.getContext("2d")
-    Object.keys(dictionary).forEach(function(element)  {
-        context.fillStyle = dictionary[element][0]
-        context.fillRect(element.split(",")[0]*pixel_resolution[0],
-            element.split(",")[1]*pixel_resolution[1],
-            Math.floor(pixel_resolution[0]), Math.floor(pixel_resolution[1]))
+    let context = canvas.getContext("2d");
+    Object.values(dictionary).forEach(function(element)  {
+        draw_pixel(context, element["x"]*pixel_resolution[0], element["y"]*pixel_resolution[1], element["type"])
     })
 }
 
-
 function update_position(x, y) {
     document.getElementById("position").innerText = "X: " + Math.floor(x/pixel_resolution[0]) + ", Y: " + Math.floor(y/pixel_resolution[1])
-
 }
 
 
-function draw_pixel(canvas, event, x, y) {
-    context = canvas.getContext("2d")
-    context.fillStyle = drawing_colour;
+function draw_pixel(context, x, y, type) {
+    context.fillStyle = Colours[type];
     context.fillRect(x, y, Math.floor(pixel_resolution[0]), Math.floor(pixel_resolution[1]));
-    return [Math.floor(x/pixel_resolution[0]).toString() + "," + Math.floor(y/pixel_resolution[1]).toString(), [drawing_colour]]
+    x = Math.floor(x/pixel_resolution[0]);
+    y = Math.floor(y/pixel_resolution[1])
+    return [x.toString() + "," + y.toString(), {"x": x, "y": y, "type": type}]
 }
 
 function draw() {
     canvas = document.getElementById('camera_canvas');
+    context = canvas.getContext("2d");
     canvas.addEventListener("mousedown", function() {
         drawing = true
     })
@@ -76,9 +73,9 @@ function draw() {
         y = e["layerY"]
         update_position(x, y)
         if (drawing) {
-            let element = draw_pixel(canvas, e, x, y)
+            let element = draw_pixel(context, x, y, drawing_colour)
             resize_window()
-            if(drawing_colour === Colours.EMPTY) {
+            if(drawing_colour === "WALL") {
                 delete dictionary[element[0]]
             }
             else {
@@ -89,8 +86,22 @@ function draw() {
     }, {passive: true, capture: true})
 }
 
+function sample() {
+    let context = canvas.getContext("2d");
+    Object.values(dictionary).forEach(function(element)  {
+        draw_pixel(context, element["x"]*pixel_resolution[0], element["y"]*pixel_resolution[1], element["type"])
+    })
+}
+
 window.onload = window.onresize = function() {
     resize_window()
 }
+
+function send_dictionary() {
+    socket.emit("canvas", {"dictionary": dictionary})
+}
+
+socket = io();
+
 
 draw()
