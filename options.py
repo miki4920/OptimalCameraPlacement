b1680c7dc3ff892295caf37580d711d36346f789
Nicolas Grobelny <miki4920@gmail.com>
2021-11-29 14:50:19 +0000

    Changed complementary colours to invert luminescence only

--- options.py
+++ options.py
@@ -2,7 +2,7 @@ import colorsys
 
 
 options = {
-    "WALL": "rgb(18,18,18)",
+    "WALL": "rgb(31,40,51)",
     "EMPTY": "rgb(255,255,255)",
     "CAMERA": "rgb(127,255,127)",
     "SAMPLE": "rgb(220,0,0)",
@@ -10,13 +10,20 @@ options = {
 }
 
 
-def get_complementary_colour(colour):
-    colour = colour[3:]
-    colour = colour.strip("()")
-    colour = colour.split(",")
-    colour = list(map(int, colour))
-    colour = list(colorsys.rgb_to_hls(*colour))
-    colour[0] = (colour[0] + 0.5) % 1.0
-    colour[1] = 255-colour[1]
-    colour = colorsys.hls_to_rgb(*colour)
-    return f"rgb{colour}"
+def get_complementary_colour(base, colour):
+    base_hsl = base[3:]
+    base_hsl = base_hsl.strip("()")
+    base_hsl = base_hsl.split(",")
+    base_hsl = list(map(int, base_hsl))
+    base_hsl = list(colorsys.rgb_to_hls(*base_hsl))
+    print(base_hsl[1])
+    if base_hsl[1] > 100:
+        colour = colour[3:]
+        colour = colour.strip("()")
+        colour = colour.split(",")
+        colour = list(map(int, colour))
+        colour = list(colorsys.rgb_to_hls(*colour))
+        colour[1] = 255-colour[1]
+        colour = colorsys.hls_to_rgb(*colour)
+        return f"rgb{colour}"
+    return colour
