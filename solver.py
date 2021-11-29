17cb27b2230d2ae05add119d9f7a54a7ec5c033c
Nicolas Grobelny <miki4920@gmail.com>
2021-11-29 12:36:36 +0000

    Separated greedy algorithm into 2 functions. Added supporting parameters

--- solver.py
+++ solver.py
@@ -8,7 +8,9 @@ class Solver:
         self.cameras = cameras
         self.evaluator = Evaluator(board)
         self.orientations = (0, 90, 180, 270)
-        self.number_of_cameras = 1
+        self.number_of_cameras = 2
+        self.visible = 1
+        self.overlapping = -1
 
     @staticmethod
     def get_maximum_values(solution_list):
@@ -17,6 +19,19 @@ class Solver:
             if solution_list[i]["score"] != highest_score:
                 return solution_list[0:i]
         return solution_list
+
+    def greedy_element(self, scores, camera_position, sample, camera, observed_nodes, orientation):
+        information = scores.get((camera_position.coordinates_hash, orientation))
+        if not information:
+            scores[(camera_position.coordinates_hash, orientation)] = {
+                "camera_position": camera_position.coordinates.tolist(),
+                "score": 0,
+                "camera": {**camera, **{"orientation": orientation}},
+                "nodes": set()}
+        scores[(camera_position.coordinates_hash, orientation)][
+            "score"] += self.visible if sample.coordinates_hash not in observed_nodes else self.overlapping
+        scores[(camera_position.coordinates_hash, orientation)]["nodes"].add(sample.coordinates_hash)
+        return scores
 
     def greedy_algorithm(self):
         observed_nodes = set()
@@ -28,17 +43,11 @@ class Solver:
                     for sample in self.evaluator["SAMPLE"]:
                         for orientation in self.evaluator.visible(camera_position.coordinates, sample.coordinates,
                                                                   camera, self.orientations):
-                            information = scores.get((camera_position.coordinates_hash, orientation))
-                            if not information:
-                                scores[(camera_position.coordinates_hash, orientation)] = {"camera_position": camera_position.coordinates.tolist(),
-                                                                                           "score": 0,
-                                                                                           "camera": camera,
-                                                                                           "nodes": [],
-                                                                                           "orientation": orientation}
-                            scores[(camera_position.coordinates_hash, orientation)]["score"] += 1
-                            scores[(camera_position.coordinates_hash, orientation)]["nodes"].append(sample.coordinates.tolist())
+                            scores = self.greedy_element(scores, camera_position, sample, camera, observed_nodes, orientation)
             scores = sorted(scores.values(), key=lambda item: item["score"], reverse=True)
             scores = self.get_maximum_values(scores)
             selected_camera = choice(scores)
+            observed_nodes = observed_nodes.union(selected_camera["nodes"])
+            selected_camera["nodes"] = [self.evaluator[element].coordinates_list for element in selected_camera["nodes"]]
             cameras.append(selected_camera)
-            print(selected_camera)
+        return cameras
