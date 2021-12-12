c90b21c08759eab4901a372530ce7e2642f1cadf
Nicolas Grobelny <miki4920@gmail.com>
2021-12-12 18:46:34 +0000

    Added selection methods

--- algorithms/genetic_algorithm.py
+++ algorithms/genetic_algorithm.py
@@ -12,6 +12,8 @@ class Parent:
 class GeneticAlgorithm(Solver):
     def __init__(self, board, cameras):
         self.population = 90
+        self.generations = 100
+        self.k = 2
         super().__init__(board, cameras)
 
     def evaluate_cameras(self):
@@ -48,12 +50,33 @@ class GeneticAlgorithm(Solver):
                         seen_samples.add(sample.coordinates_hash)
         Parent.score = len(seen_samples)/count
 
+    def tournament_selection(self, parents):
+        tournament_population = []
+        for i in range(0, self.k):
+            tournament_population.append(choice(parents))
+        return max(tournament_population, key=lambda parent: parent.score)
+
+    def crossover(self, parent_one, parent_two):
+        pass
+
+    def mutate(self, child):
+        pass
+
     def solve(self):
         self.evaluate_cameras()
         self.evaluator["CAMERA"] = list(self.evaluator["CAMERA"])
         parents = self.initialise_population()
         [self.evaluate_parent(parent) for parent in parents]
-        parents = sorted(parents, key=lambda parent: parent.score, reverse=True)
+        for i in range(0, self.generations):
+            children = []
+            while len(children) != self.population:
+                parent_one = self.tournament_selection(parents)
+                parent_two = self.tournament_selection(parents)
+                child_one, child_two = self.crossover(parent_one, parent_two)
+                child_one = self.mutate(child_one)
+                child_two = self.mutate(child_two)
+                children.extend([child_one, child_two])
+
 
 
 
