8b52400f1218b8f0d7bd947a3482c5488da3d505
Nicolas Grobelny <miki4920@gmail.com>
2022-01-16 20:53:17 +0000

    Made CameraNode a subclass of Node

--- algorithms/solver.py
+++ algorithms/solver.py
@@ -1,13 +1,11 @@
 from random import choice
-from environment import Evaluator
+from environment import Evaluator, Node
 
 
-class CameraNode:
-    def __init__(self, position, orientation, camera):
+class CameraNode(Node):
+    def __init__(self, parent, camera, orientation):
+        super().__init__(parent.coordinates, parent.node_type)
         self.camera = camera
-        self.position = position
-        self.position_hash = self.position.tobytes()
-        self.position_list = self.position.tolist()
         self.orientation = orientation
         self.camera_set = set()
         self.final_camera_set = set()
@@ -21,15 +19,12 @@ class CameraNode:
     def __lt__(self, other):
         return len(self) < len(other)
 
-    def __hash__(self):
-        return str(self.position_hash)+str(self.orientation)
-
-    def json(self, evaluator):
-        return {"x": self.position_list[0],
-                "y": self.position_list[1],
+    def json(self):
+        return {"x": self.coordinates_tuple[0],
+                "y": self.coordinates_tuple[1],
                 "orientation": self.orientation,
                 "camera": self.camera,
-                "nodes": [f"({evaluator[node].coordinates_list[0]}, {evaluator[node].coordinates_list[1]})"
+                "nodes": [f" {node}"
                           for node in self.final_camera_set]}
 
     def update(self, other):
@@ -46,6 +41,9 @@ class Solver:
         self.evaluator = Evaluator(board)
         self.camera_nodes = sorted(filter(lambda node: len(node) > 0, self.evaluated_cameras()))
 
+    def serialize_to_json(self, cameras):
+        return [node.json() for node in cameras]
+
     def evaluated_cameras(self):
         cameras_dictionary = {}
         for camera in self.cameras:
@@ -55,15 +53,9 @@ class Solver:
                     sample = self.evaluator[sample]
                     for orientation in self.evaluator.visible(camera_node.coordinates, sample.coordinates,
                                                               camera, self.orientations):
-                        hash_string = str(camera_node.coordinates_hash) + str(orientation)
-                        if hash_string not in cameras_dictionary:
-                            cameras_dictionary[hash_string] = CameraNode(camera_node.coordinates, orientation, camera)
-                        cameras_dictionary[hash_string].add_node(sample.coordinates_hash)
+                        camera_hash = camera_node.hash() + (orientation, )
+                        if camera_hash not in cameras_dictionary:
+                            cameras_dictionary[camera_hash] = CameraNode(camera_node, camera, orientation)
+                        cameras_dictionary[camera_hash].add_node(sample.hash())
         return list(cameras_dictionary.values())
-
-    def update_evaluated_cameras(self, selected_node):
-        selected_node.final_camera_set = selected_node.camera_set.copy()
-        [node.update(selected_node) for node in self.camera_nodes]
-        self.camera_nodes = [node for node in self.camera_nodes if len(node) > 0]
-        self.camera_nodes = sorted(self.camera_nodes)
 
