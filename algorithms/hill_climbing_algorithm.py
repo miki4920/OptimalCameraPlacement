d98838b9e5e4fe73ff7055f72bfc6441b5e8ce8e
Nicolas Grobelny <miki4920@gmail.com>
2022-01-24 21:22:50 +0000

    Improved performance of the hill climbing algorithm by removing additional set

--- algorithms/hill_climbing_algorithm.py
+++ algorithms/hill_climbing_algorithm.py
@@ -7,13 +7,12 @@ class HillClimbingAlgorithm(Solver):
     def __init__(self, board, cameras):
         super().__init__(board, cameras)
 
-    def get_maximum_values(self):
+    def get_maximum_indexes(self):
         score = self.camera_nodes[-1]
         index = self.camera_nodes.index(score)
-        return self.camera_nodes[index-len(self.camera_nodes):]
+        return choice(range(index, len(self.camera_nodes)))
 
     def update_evaluated_cameras(self, selected_node):
-        selected_node.final_camera_set = selected_node.camera_set.copy()
         [node.update(selected_node) for node in self.camera_nodes]
         self.camera_nodes = [node for node in self.camera_nodes if len(node) > 0]
         self.camera_nodes = sorted(self.camera_nodes)
@@ -21,10 +20,11 @@ class HillClimbingAlgorithm(Solver):
     def solve(self):
         cameras = []
         while self.camera_nodes:
-            camera = choice(self.get_maximum_values())
+            index = self.get_maximum_indexes()
+            camera = self.camera_nodes[index]
             cameras.append(camera)
+            del self.camera_nodes[index]
             self.update_evaluated_cameras(camera)
-        coverage = round(sum([len(camera.final_camera_set) for camera in cameras]) / len(self.evaluator["SAMPLE"]) * 100, 2)
+        coverage = round(sum([len(camera.camera_set) for camera in cameras]) / len(self.evaluator["SAMPLE"]) * 100, 2)
         coverage = coverage/len(cameras)
-        cameras = self.serialize_to_json(cameras)
         return cameras, coverage
