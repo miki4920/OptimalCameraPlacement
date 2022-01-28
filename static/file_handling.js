098bf20fb88b37fbd9393f636a37ee22a776eeb0
Nicolas Grobelny <miki4920@gmail.com>
2022-01-28 16:01:20 +0000

    Implemented separate file handling. Made changes from the original format for simplification

--- static/file_handling.js
+++ static/file_handling.js
@@ -1,4 +1,54 @@
-class FileHandler {
-
-
+function process_text(text) {
+    text = text.split("\n");
+    let width = parseInt(text[0]);
+    let height = parseInt(text[1]);
+    let dimensions = {width, height}
+    text = text.slice(2, text.length - 1)
+    for (let i = 0; i < text.length; i++) {
+        text[i] = text[i].split(" ");
+        text[i] = text[i].slice(0, 3);
+        text[i] = text[i].map(number => parseInt(number));
+    }
+    return {text, dimensions};
 }
+
+class FileProcessor {
+    constructor() {
+        this.files = {}
+        this.dimensions = {}
+    }
+    set_file(e) {
+        let reader = new FileReader();
+        let type = e.currentTarget.file_type;
+        reader.onload = function (e) {
+            let read_file = process_text(e.target.result);
+            file_processor.files[type] = read_file.text;
+            file_processor.dimensions = read_file.dimensions
+        };
+        reader.readAsText(e.target.files[0]);
+    }
+
+    update_environment() {
+        if (Object.keys(this.files).length !== 2) {
+            return
+        }
+        environment.create_board(this.dimensions.width, this.dimensions.height, environment.default_type);
+        for (const [key, value] of Object.entries(this.files)) {
+            environment.selected_type = key;
+            for (let i = 0; i < value.length; i++) {
+                environment.board[value[i][0]][value[i][1]].update(environment.selected_type)
+            }
+        }
+        environment.update_canvas();
+        environment.change_selected_type(document.getElementById("EMPTY"));
+    }
+}
+
+let file_processor = new FileProcessor();
+
+let camera = document.getElementById('camera_upload');
+camera.addEventListener('change', file_processor.set_file, false);
+camera.file_type = "CAMERA";
+let sample = document.getElementById('sample_upload');
+sample.addEventListener('change', file_processor.set_file, false)
+sample.file_type = "SAMPLE";
