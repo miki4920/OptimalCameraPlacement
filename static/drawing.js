0ca37cb6a652584e51e293ca3caabbfbc4329ebf
Nicolas Grobelny <miki4920@gmail.com>
2022-01-28 16:02:49 +0000

    Added a separate drawing layer to allow support for different tools

---
+++ static/drawing.js
@@ -0,0 +1,22 @@
+function set_event_listeners() {
+    environment.canvas.addEventListener("mousedown", function () {
+        drawing = true
+    })
+    environment.canvas.addEventListener("mouseup", function () {
+        drawing = false
+    })
+    environment.canvas.addEventListener("mousemove", function (e) {
+        let x = environment.normalise(e["layerX"], 0, "x");
+        let y = environment.normalise(e["layerY"], 1, "y");
+        update_information(environment.board[x][y]);
+        if (drawing) {
+            environment.board[x][y].update(environment.selected_type)
+            environment.update_canvas()
+        }
+    })
+    window.onload = window.onresize = function () {
+        environment.update_canvas();
+    }
+}
+
+set_event_listeners()
