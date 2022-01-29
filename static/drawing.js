4ba8a9c91de10742fb07c6b688a74747f417753b
Nicolas Grobelny <miki4920@gmail.com>
2022-01-29 14:49:35 +0000

    Added line drawing tool

--- static/drawing.js
+++ static/drawing.js
@@ -1,25 +1,132 @@
+let default_size = 11;
+
 class DrawingTool {
     constructor() {
         this.canvas = document.getElementById("camera_canvas");
         this.pixel_resolution = [];
         this.drawing = false;
+        this.drawing_types = {"POINT": this.draw_point, "LINE": this.draw_line, "FILL": this.fill}
+        this.drawing_type = "POINT"
+        this.previous_line_point = {}
+        this.colour_type = "EMPTY";
+        this.environment = new Environment();
+        this.environment.create_board(default_size)
     }
 
-    start_drawing() {
-        this.drawing = true;
+    sample() {
+        this.environment.sample()
+        this.update_canvas()
+    }
+
+    refresh_canvas() {
+        let self = this
+        this.environment.board.forEach((row) => {
+            row.forEach((element) => {
+                this.draw_point_on_canvas(element.x, element.y, element.type, self)
+            })
+        })
+    }
+
+    update_canvas() {
+        this.canvas.width = this.canvas.clientWidth;
+        this.canvas.height = this.canvas.clientHeight;
+        this.pixel_resolution = [this.canvas.clientWidth / this.environment.size,
+            this.canvas.clientHeight / this.environment.size];
+        this.refresh_canvas();
+    }
+
+    create_board() {
+        let size = document.getElementById("size").value
+        size = size && !isNaN(size) ? parseInt(size) : default_size;
+        this.environment.create_board(size)
+        this.update_canvas()
+    }
+
+    start_drawing(e) {
+        if(e.button === 0) {
+            this.drawing = true;
+            this.draw(e)
+        }
+        else if(e.button === 2) {
+            this.previous_line_point = {}
+        }
+
     }
 
     stop_drawing() {
         this.drawing = false;
     }
 
+    change_selected_colour(id) {
+        let elements = Array.from(document.getElementsByClassName("active_option"))
+        elements.forEach((element) => {
+            element.classList.remove("active_option")
+        })
+        document.getElementById(id).classList.add("active_option");
+        this.colour_type = id
+    }
+
+    change_selected_drawing(id) {
+        let elements = Array.from(document.getElementsByClassName("active_drawing"))
+        elements.forEach((element) => {
+            element.classList.remove("active_drawing")
+        })
+        document.getElementById(id).classList.add("active_drawing");
+        this.drawing_type = id
+        if(this.drawing_type !== "LINE") {
+            this.previous_line_point = {}
+        }
+    }
+
+    normalise(value, index) {
+        let result = Math.floor(value / this.pixel_resolution[index]);
+        return result > 0 ? result : 0;
+    }
+
+    draw_point_on_canvas(x, y, type) {
+        let context = this.canvas.getContext("2d");
+        context.fillStyle = Colours[type];
+        context.fillRect(x * this.pixel_resolution[0], y * this.pixel_resolution[1],
+            this.pixel_resolution[0] - 1, this.pixel_resolution[1] - 1);
+    }
+
+    draw_point(x, y, type, self) {
+        self.environment.board[x][y].update(type)
+    }
+
+    line(x0, y0, x1, y1, type) {
+       let dx = Math.abs(x1 - x0);
+       let dy = Math.abs(y1 - y0);
+       let sx = (x0 < x1) ? 1 : -1;
+       let sy = (y0 < y1) ? 1 : -1;
+       let err = dx - dy;
+
+       let self = this;
+       while(true) {
+          this.draw_point(x0, y0, type, self);
+          if ((x0 === x1) && (y0 === y1)) break;
+          let e2 = 2*err;
+          if (e2 > -dy) { err -= dy; x0  += sx; }
+          if (e2 < dx) { err += dx; y0  += sy; }
+       }
+}
+
+    draw_line(x, y, type, self) {
+        if(Object.keys(self.previous_line_point).length !== 0 && self.previous_line_point["type"] === type) {
+            self.line(self.previous_line_point["x"], self.previous_line_point["y"], x, y, type)
+        }
+        self.previous_line_point = {"drawing_type": "LINE_START", "type": type, "x": x, "y": y}
+        self.draw_point(x, y, type, self)
+    }
+
     draw(e) {
-        let x = environment.normalise(e["layerX"], 0);
-        let y = environment.normalise(e["layerY"], 1);
-        update_information(environment.board[x][y]);
+        let x = this.normalise(e["layerX"], 0);
+        let y = this.normalise(e["layerY"], 1);
+        update_information(this.environment.board[x][y]);
         if (this.drawing) {
-            environment.board[x][y].update(environment.selected_type)
-            environment.update_canvas()
+            let self = this
+            this.drawing_types[this.drawing_type](x, y, this.colour_type, self)
+            this.update_canvas()
         }
     }
 
@@ -28,9 +135,9 @@ class DrawingTool {
 
 let drawing_tool = new DrawingTool();
 
-environment.canvas.addEventListener("mousedown", drawing_tool.start_drawing)
-    environment.canvas.addEventListener("mouseup", drawing_tool.stop_drawing)
-    environment.canvas.addEventListener("mousemove", drawing_tool.draw)
+drawing_tool.canvas.addEventListener("mousedown", function(e) {drawing_tool.start_drawing(e)})
+drawing_tool.canvas.addEventListener("mouseup", function() {drawing_tool.stop_drawing()})
+drawing_tool.canvas.addEventListener("mousemove", function(e) {drawing_tool.draw(e)})
 window.onload = window.onresize = function () {
-        environment.update_canvas();
+    drawing_tool.update_canvas();
 }
