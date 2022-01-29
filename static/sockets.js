6986974e2d9d41cc89da5d20717a507dc4bb472c
Nicolas Grobelny <miki4920@gmail.com>
2022-01-29 14:58:28 +0000

    Fixed sockets not working due to environment and drawing tool changes

--- static/sockets.js
+++ static/sockets.js
@@ -1,9 +1,9 @@
 let socket = io("http://127.0.0.1:5000/");
 
 function send_environment() {
-    environment.clean_selection();
+    drawing_tool.environment.clean_selection();
     let algorithm = document.getElementById("algorithm").value;
-    socket.emit("environment", {"board": environment.board, "cameras": environment.cameras, "algorithm": algorithm});
+    socket.emit("environment", {"board": drawing_tool.environment.board, "cameras": drawing_tool.environment.cameras, "algorithm": algorithm});
 }
 
 socket.on("update_board", (message) => {
@@ -12,9 +12,9 @@ socket.on("update_board", (message) => {
     solution.forEach((element) => {
         let x = element["x"];
         let y = element["y"];
-        environment.board[x][y].update("SELECTED", element);
+        drawing_tool.environment.board[x][y].update("SELECTED", element);
     })
-    environment.update_canvas();
+    drawing_tool.update_canvas();
     document.getElementById("coverage").innerHTML = "Total Coverage: " + Math.floor(message["coverage"] * solution.length) + " %"
     document.getElementById("coverage_per_camera").innerHTML = "Average Coverage per Camera: " + message["coverage"] + " %"
 })
