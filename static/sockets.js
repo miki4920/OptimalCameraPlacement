f1907cc2d604b6d1c481fc95a1752c252759e2b7
miki4920 <miki4920@gmail.com>
2022-03-02 14:58:25 +0000

    Added unseen samples that are highlighted after each execution

--- static/sockets.js
+++ static/sockets.js
@@ -15,11 +15,22 @@ function send_environment() {
 socket.on("update_board", (message) => {
     message = JSON.parse(message)
     let solution = message["solution"]
+    let nodes = []
     solution.forEach((element) => {
         let x = element["x"];
         let y = element["y"];
+        nodes.push(...element["nodes"])
         drawing_tool.environment.board[x][y].update("SELECTED", element);
     })
+    for (let x = 0; x < drawing_tool.environment.size; x++) {
+            for (let y = 0; y < drawing_tool.environment.size; y++) {
+                if(drawing_tool.environment.board[x][y].type === "SAMPLE") {
+                    if(!nodes.includes(" ("+x + ", " + y + ")")) {
+                        drawing_tool.environment.board[x][y].update("UNSEEN")
+                    }
+                }
+            }
+        }
     drawing_tool.update_canvas();
     document.getElementById("coverage").innerHTML = "Total Coverage: " + Math.floor(message["coverage"] * solution.length) + " %"
     document.getElementById("coverage_per_camera").innerHTML = "Average Coverage per Camera: " + message["coverage"] + " %"
